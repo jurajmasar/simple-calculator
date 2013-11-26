fb6cba6a4fefcc8da456d0b96cc15da9cc4af3c3
Juraj Masar <mail@jurajmasar.com>
2013-11-26 23:05:17 +0100

    Initial commit.

---
+++ CodEx.java
@@ -0,0 +1,213 @@
+/* Use the default unnamed package, i.e. do not use the "package" statement. */
+
+import java.io.*;
+import java.math.BigDecimal;
+
+/** DU 1. 
+ * 
+ * @author Juraj Masar <mail@jurajmasar.com>
+ * @date 26 November 2013
+ */
+public class CodEx {
+    /**
+     * Result of the evaluation of the last expression.
+     */
+    static BigDecimal last = new BigDecimal(0);
+    
+    /** 
+     * Rounding mode used for printing out the number.
+     */
+    static final int ROUNDING_MODE = BigDecimal.ROUND_DOWN;
+    
+    /**
+     * Precision to be used in the next evaluation.
+     */
+    static int precision = 20;    
+
+    /**
+     * Type of a node in an expression evaluation tree.
+     */
+    enum ExpressionType {
+        VALUE, ADDITION, SUBTRACTION, MULTIPLICATION, DIVISION
+    }
+
+    /**
+     * Expression to be thrown if an invalid expression is being evaluated.
+     */
+    static class InvalidExpressionException extends Exception {}
+    
+    
+    /**
+     * Class representing a node in an tree for evaluation of a mathematical expression.
+     */
+    static class Expression {
+        /**
+         * Type of the expression.
+         */
+        ExpressionType type;
+        
+        /**
+         * Left and right sons of the node being used if the ExpressionType is a binomial operation (+,-,*,/).
+         */
+        Expression left, right;
+        
+        /**
+         * Numeric value of the node if the ExpressionType being used is VALUE.
+         */
+        BigDecimal value;
+               
+        /**
+         * Recursively parses a given String expression into a tree of Expression classes.
+         */
+        public Expression(String input) throws InvalidExpressionException {
+            // remove whitespace 
+            StringBuilder sb = new StringBuilder(input.trim());
+
+            // validate input
+            if (sb == null || sb.length() == 0) throw new InvalidExpressionException();                       
+            
+            // number of opening brackets that we encountered
+            int bracketLevel = 0;
+            
+            //
+            // remove enclosing brackets
+            //
+            
+            boolean isEnclosedInBrackets = true;
+                        
+            while (isEnclosedInBrackets && sb.charAt(0) == '(' && sb.charAt(sb.length()-1) == ')') {
+                bracketLevel = 0;
+                
+                // iterate over expression and decide whether it is enclosed in brackets
+                // e.g. (a) is OK but (a)/(b) is not, although both pass the condition in while                
+                for (int i = 0; i < sb.length(); i++) {
+                    if (sb.charAt(i) == '(') bracketLevel++;
+                    if (sb.charAt(i) == ')') bracketLevel--;
+                    
+                    if (bracketLevel == 0 && i != 0 && i != sb.length() - 1) {
+                        isEnclosedInBrackets = false;
+                        break;
+                    }
+                }
+                
+                if (isEnclosedInBrackets) {
+                    // remove brackets                    
+                    sb.deleteCharAt(0).deleteCharAt(sb.length()-1);
+                }
+            }
+            
+            //
+            // check for + and -
+            //
+            
+            bracketLevel = 0;
+            for (int i = 0; i < sb.length(); i++) {
+                    if (sb.charAt(i) == '(') bracketLevel++;
+                    if (sb.charAt(i) == ')') bracketLevel--;
+
+                    if ((sb.charAt(i) == '+' || sb.charAt(i) == '-') && bracketLevel == 0 && i > 0) {
+                        if (sb.charAt(i) == '+') type = ExpressionType.ADDITION;
+                        if (sb.charAt(i) == '-') type = ExpressionType.SUBTRACTION;
+                        
+                        left = new Expression(sb.substring(0, i));
+                        right = new Expression(sb.substring(i+1, sb.length()));
+                        
+                        return;
+                    }
+            }
+            
+            //
+            // check for * and /
+            //
+            
+            bracketLevel = 0;
+            for (int i = 0; i < sb.length(); i++) {
+                    if (sb.charAt(i) == '(') bracketLevel++;
+                    if (sb.charAt(i) == ')') bracketLevel--;
+
+                    if ((sb.charAt(i) == '*' || sb.charAt(i) == '/') && bracketLevel == 0) {
+                        if (sb.charAt(i) == '*') type = ExpressionType.MULTIPLICATION;
+                        if (sb.charAt(i) == '/') type = ExpressionType.DIVISION;
+                        
+                        left = new Expression(sb.substring(0, i));
+                        right = new Expression(sb.substring(i+1, sb.length()));
+                        
+                        return;
+                    }
+            }            
+            
+            //
+            // check for "last"
+            //
+            
+            if ("last".equals(sb.toString())) {
+               type = ExpressionType.VALUE;
+               value = last;
+               
+               return;
+            }
+            
+            //
+            // check if the expression is non-empty alphanumeric string
+            //
+            
+            try {
+                type = ExpressionType.VALUE;
+                value = new BigDecimal(sb.toString());
+            } catch (NumberFormatException ex) {
+                throw new InvalidExpressionException();
+            }
+        }
+        
+        /**
+         * Return numeric value of the Expression tree with the root in the current node.
+         */
+        public BigDecimal getResult() {
+            switch (type) {
+                case ADDITION:
+                    return left.getResult().add(right.getResult());
+                case SUBTRACTION:
+                    return left.getResult().subtract(right.getResult());
+                case MULTIPLICATION:
+                    return left.getResult().multiply(right.getResult());
+                case DIVISION:
+                    return left.getResult().divide(right.getResult(), precision, ROUNDING_MODE);
+                default:
+                    return value;
+            }
+        }
+    }
+
+    /**
+     * Entry point for the program.
+     * 
+     * Reads the standard input line by line and tries to evaluate the given mathematical expression on every line.
+     */
+    public static void main(String[] argv) {
+        try {
+            BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+            String line;
+            while ((line = input.readLine()) != null) {
+                try {
+                    line = line.trim();                    
+                    if (line.length() > 9 && "precision".equals(line.substring(0,9))) {
+                        // change precision
+                        precision = Integer.parseInt(line.substring(9, line.length()).trim());
+                    } else {
+                        // handle arithmetic expression
+                        last = new Expression(line).getResult().stripTrailingZeros();
+                        // do not use scientific notation e.g. "1E2"
+                        System.out.println(last.toPlainString());
+                    }
+                } catch (InvalidExpressionException e) {
+                    System.out.println("CHYBA");
+                    last = new BigDecimal(0);
+                }
+            }
+            System.out.println();
+        } catch (IOException ex) {
+            System.out.println("IOException occured, exiting.");
+        }
+    }
+}
+
--- README.md
+++ README.md
@@ -1,4 +1,62 @@
-simple-calculator
-=================
+Simple calculator with unlimited precision
+==========================================
 
-Simple calculator with unlimited precision.
+> This small project is a solution for an assignment for my Java class at Charles University in Prague.
+
+### Usage instructions
+
+1. checkout the repository
+2. compile the CodEx class
+    javac CodEx.java
+3. run the CodEx.class
+    java CodEx
+4. run "tests" with
+    javac CodEx.java && java CodEx < INPUT > CURRENT_OUTPUT && diff CURRENT_OUTPUT OUTPUT
+
+Assignment: simple calculator with unlimited precision
+------------------------------------------------------
+
+Create a simple calculator, which allows computing with unlimited precision. The program reads data from the standard input and prints to the standard output. The calculator can add, subtract, multiply and divide. Priority of the operators is as usual and can be changed by parentheses. The input format of the numbers is usual with a decimal dot (integer numbers are of course also allowed). Numbers can be negative (i.e. unary minus can be also used).
+
+Expressions are separated by a new line. The program always prints out the result of the expression. The result of the expression is also stored to the special variable last, which can be used in the subsequent expression (for the first expression, the last variable is set to zero). The elements on a single line are separated by an arbitrary number of white spaces (space, tab,. . . ).
+
+If the expression does not correspond to the above rules, the program prints out the text CHYBA. The last variable is then set to zero.
+
+Initially, the program computes with the precision set to 20 digits after the decimal point. The precision can be changed via the precision command.
+
+#### Examples:
+
+3+2 ⇒5 2∗(3+3) ⇒12 (2−3∗(1+2)) ⇒−7 2∗3 ⇒6 last−3 ⇒3
+10 ∗ last
+1.2 + 4.3
+1/3
+1000000000 ∗ 1000000000 precision 30
+1/3 3∗∗5 last∗3
+⇒ 30
+⇒ 5.5
+⇒ 0.33333333333333333333 ⇒ 1000000000000000000
+⇒ 0.333333333333333333333333333333 ⇒ CHYBA
+⇒ 0
+
+## License 
+
+    Released under MIT license.
+
+    Permission is hereby granted, free of charge, to any person obtaining
+    a copy of this software and associated documentation files (the
+    "Software"), to deal in the Software without restriction, including
+    without limitation the rights to use, copy, modify, merge, publish,
+    distribute, sublicense, and/or sell copies of the Software, and to
+    permit persons to whom the Software is furnished to do so, subject to
+    the following conditions:
+
+    The above copyright notice and this permission notice shall be
+    included in all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+    MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+    NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+    LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+    OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+    WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
